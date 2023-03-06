1716565f7a82f9f66ef4d1fd45e47aafbc659a2a
12101111 <w12101111@gmail.com>
2023-03-07 00:09:45 +0800

    Add patch check_stack_overflow.patch

--- quickjs.c
+++ quickjs.c
@@ -1590,9 +1590,7 @@ static inline uintptr_t js_get_stack_pointer(void)
 
 static inline BOOL js_check_stack_overflow(JSRuntime *rt, size_t alloca_size)
 {
-    uintptr_t sp;
-    sp = js_get_stack_pointer() - alloca_size;
-    return unlikely(sp < rt->stack_limit);
+    return unlikely(js_get_stack_pointer() < rt->stack_limit + alloca_size);
 }
 #endif
 
